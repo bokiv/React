674b9789986cf20cff4bca307452e1ddfa66d199
bokiv <borislavivanisevic@gmail.com>
2021-08-27 07:47:20 +0200

    commit 26.08.

--- user-list-fetch/src/App.js
+++ user-list-fetch/src/App.js
@@ -3,14 +3,18 @@ import React, {Fragment, Component} from 'react';
 import {Header} from './components/Header/Header';
 import {Footer} from './components/Footer/Footer';
 import {Users} from './components/Users/users';
-
+import {getUsers} from './services/user-services'
 
 
 class App extends Component {
    constructor(props){
      super(props);
      this.state = {
-       isListView: true
+       isListView: true,
+       users : [],
+       isRefresh : true,
+       search : "",
+       filteredUsers : []
      };
      this.onLayoutToggle = this.onLayoutToggle.bind(this);
    }
@@ -21,12 +25,53 @@ class App extends Component {
      });
    };
 
+   onBtnRefresh = () => {
+    getUsers().then(users => this.setState({users : users}))
+     this.setState({
+       isRefresh : !this.state.isRefresh
+     });
+   };
+ 
+   onTyping = (e) => {
+      this.setState({
+        search : e.target.value,
+        
+      });
+    console.log(e.target.value) 
+   }
+
+   getFilteredUser = () => {
+     this.setState({
+      filteredUsers : this.state.filterUsers
+     })
+     const filterUsers = getUsers().filter(user => user.name.first.toLowerCase().includes(this.search.toLowerCase()));
+     let filteredUsers = filterUsers;
+     return filteredUsers
+   }
+   
+  //  onTyping = () => {
+  //   getUsers().filter((search) => {
+  //     if(this.search === '') {
+  //       return search
+  //     }else if (this.users.name.first.toLowercase().includes(this.search.toLowercase())){
+  //       return search
+  //     }
+  //   }).then(users => this.setState({users : users}))
+  //  }
+
+   componentDidMount(){
+    getUsers().then(users => this.setState({users : users}))
+  }
+
+
    render() { 
-    console.log('App ', this.state.isListView);
+    console.log('App ',  this.state.isRefresh);
   return (
     <Fragment> 
-    <Header isListView={this.state.isListView} onLayoutToggle={this.onLayoutToggle}/> 
-    <Users isListView={this.state.isListView}/> 
+    <Header isListView={this.state.isListView} onLayoutToggle={this.onLayoutToggle} users={this.state.users} 
+            isRefresh={this.state.isRefresh} onBtnRefresh={this.onBtnRefresh} onTyping={this.onTyping} search={this.state.search} /> 
+    <Users isListView={this.state.isListView} users={this.state.users}  isRefresh={this.state.isRefresh} onTyping={this.onTyping} 
+            search={this.state.search} getFilteredUser={this.getFilteredUser} /> 
     <Footer/>
     </Fragment>
   );
--- user-list-fetch/src/components/Header/Header.jsx
+++ user-list-fetch/src/components/Header/Header.jsx
@@ -1,18 +1,50 @@
 import React from "react";
 
-export const Header = ({ isListView, onLayoutToggle }) => {
-  console.log("Header: ", isListView);
+export const Header = ({
+  isListView,
+  onLayoutToggle,
+  isRefresh,
+  onBtnRefresh,
+  onTyping,
+  search
+}) => {
+  console.log("Header: ", isRefresh);
+
+  // const refreshPage = () => {
+  //   window.location.reload(onBtnRefresh);
+  // };
   return (
-    <nav>
+    <nav style={{minHeight: "118px"}}>
       <div className="nav-wrapper">
         <a href="#!" className="brand-logo center">
           React Users
         </a>
-        <a href="#!" className="waves-effect waves-light btn" onClick={onLayoutToggle}>
+        <a
+          href="#!"
+          className="waves-effect waves-light btn"onClick={onLayoutToggle}>
           {" "}
           {isListView ? "List View" : "Grid View"}{" "}
         </a>
+        <a
+          href="#!"
+          className="waves-effect waves-light btn"onClick={onBtnRefresh}>
+          Refresh
+        </a>
+        <div className="center row">
+            <div className="col s12 " >
+              <div className="row" id="topbarsearch">
+                <div className="input-field col s6 s12 red-text">
+                            {/* <i className="red-text material-icons prefix">search</i> */}
+<input type="text" placeholder="search" id="autocomplete-input" className="autocomplete white-text" 
+onChange={onTyping}  value={search} />
+                  </div>
+                </div>
+              </div>
+            </div> 
+        
       </div>
+     
     </nav>
+    
   );
 };
